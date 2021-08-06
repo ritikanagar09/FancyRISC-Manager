9b315fa232bf0616ae37befb1dec6ce9fd0dcf28
Yash Keswani <yash20158@iiitd.ac.in>
2021-08-06 21:42:43 +0530

    fixed binary encoding for find_cat.

--- Simple-Assembler/reader.py
+++ Simple-Assembler/reader.py
@@ -4,21 +4,23 @@ def find_variant(line):
 	return "instruction"
 
 
-insts = json.load('instructions.json')
+insts = json.load(open('instructions.json'))
 def find_cat(cmd):
 	"finds what category a specific command belongs to, and its opcode"
 
 	params = cmd.split()
 
+	if len(params) == 0 or params[0] not in insts:
+		return {'opcode':0b11111, 'cat':'unidentified'}
 	if params[0] not in insts:  # invalid instruction type
 		return {'opcode':0b11111, 'cat':'unidentified'}
 
-	opcode = int(insts[params[0]]['opcode'])
+	opcode = int(insts[params[0]]['opcode'],base = 2)
 	cat = 'A'
 	return {'opcode':opcode, 'cat':cat}
 
 
-cats = json.loads('categories.json')
+cats = json.load(open('categories.json'))
 def encode(ctg,cmd):
 	"encodes command according to its category"
 
